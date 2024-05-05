d4d8c81f396213bc59a415bf4a0c42b4c56093f1
Isa Westling <isawestling.blogg@hotmail.com>
2024-05-05 21:03:17 +0200

    Korrigerat funktion för att skapa ny användare, lagt till token i fetch-anrop samt kontroll på om token saknas. lagt till popup vi lyckad tilläggning och rensning av input-fält.

--- src/js/create.js
+++ src/js/create.js
@@ -2,13 +2,17 @@
 
 // Importerar modul
 import { errorMsg } from "./main";
+import { url } from "./main";
+import { loadingIcon } from "./booking";
 
 // Asynkron funktion för att skapa ny användare
 export async function createUser() {
-    const url = "https://backend-moment4-1.onrender.com/api/register"; // Lagrar url för API
-    const username = document.getElementById("new-username").value; // Hämtar användarnamnet från formuläret
-    const password = document.getElementById("new-password").value; // Hämtar lösenordet från formuläret
-    const loadingEl = document.querySelector(".loader"); // Hämtar ikon för laddningssymbol
+
+    const token = localStorage.getItem("JWT"); // Hämtar token från localStorage
+    loadingIcon.style.display = "block"; // Visar laddningsikon
+
+    const username = document.getElementById("new-username"); // Hämtar användarnamnet från formuläret
+    const password = document.getElementById("new-password"); // Hämtar lösenordet från formuläret
 
     // Kontrollerar om lösenordet är minst 5 tecken
     if (password.length < 5) {
@@ -17,57 +21,58 @@ export async function createUser() {
         return;
     }
 
-    loadingEl.style.display = "block"; // Visar laddningsikon
-
     try {
         // Skapar nytt objekt för användaren
         const userInfo = {
-            username: username,
-            password: password
+            username: username.value,
+            password: password.value
         }
 
         // Skickar ett POST-anrop med fetch API till webbtjänsten med objektet som skapats för användaren
-        const response = await fetch(url, {
+        const response = await fetch(`${url}register`, {
             method: "POST",
             headers: {
-                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token,
+                "Content-Type": "application/json"
             },
             body: JSON.stringify(userInfo)
         });
 
         const data = await response.json(); // Inväntar svar och omvandlar till json
 
-        loadingEl.style.display = "none"; // Döljer laddningsikon
+        loadingIcon.style.display = "none"; // Döljer laddningsikon
 
-        // Kontrollerar om svaret var lyckat eller inte
-        if (!response.ok) {
-            console.error("Fel vid skapande av användare: ", data.error); // Skriver ut felet till konsollen om ej lyckat
+        // Kontrollerar om token saknas eller är ogiltig
+        if (response.status === 401 || response.status === 403) {
+            alert(`${result.message}. Du är inte inloggad eller din session har gått ut. Vänligen logga in igen.`); // Skriver ut felmeddelande till klienten
+            window.location.href = "login.html"; // Omdirigerar till inloggningssidan
+            return;
+            // Kontrollerar om svaret blev lyckat eller ej
+        } else if (!response.ok) {
+            console.error("Fel vid skapande av konto: ", data.error); // Skriver ut felet till konsollen om ej lyckat
             errorMsg.style.display = "flex"; // Visar felmeddelande med felet från anropet
             errorMsg.innerHTML = data.error;
             return;
-
             // Vid lyckat svar
         } else {
+            // Tömmer innehåll i inputfälten
+            username.value = "";
+            password.value = "";
 
             // Lagrar variabel för popup
             let popupMsg = document.querySelector(".popup");
             popupMsg.classList.add("show"); // Lägger till klassen show för popup när erfarenheten har skapats
-            popupMsg.innerHTML = `Användarkonto för ${username} har skapats!`; // Skapar innehållet för popupen
+            popupMsg.innerHTML = "Användarkonto har skapats"; // Skapar innehållet för popupen
 
             // Döljer popup efter 3 sekunder
             setTimeout(function () {
                 popupMsg.classList.remove("show"); // Tar bort show-klassen
                 popupMsg.innerHTML = ""; // Tömmer innehållet
-
-                // Omdirigerar användaren till startsidan efter att popupen har dolts
-                setTimeout(function () {
-                    window.location.href = "index.html";
-                }, 800); // Omdirigerar användaren till startsidan efter att popupen har dolts, 0.8 sekunders fördröjning
             }, 3000); // 3 sekunder
         }
 
         // Fångar upp ev fel
     } catch (error) {
-        console.error("Något gick fel vid inloggning: ", error);
+        console.error("Något gick fel vid skapande av konto: ", error);
     }
 }
